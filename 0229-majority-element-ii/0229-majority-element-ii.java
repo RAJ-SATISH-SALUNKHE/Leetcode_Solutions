31e43956648f43820046e184ee92c9c61f3dfcb7
Raj Salunkhe <98548147+RAJ-SATISH-SALUNKHE@users.noreply.github.com>
2023-10-05 14:00:24 +0530

    Time: 15 ms (12.36%), Space: 46.4 MB (64.44%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+
+        int threshold = nums.length/3;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++)
+        {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+            if(map.get(nums[i]) > threshold && !result.contains(nums[i]))result.add(nums[i]);
+        }
+
+        return result;
+    }
+}
